90eddbeb9b05ed512f2ba85868713f06571c5061
Kminassch <s18807016698@outlook.com>
2020-07-16 20:09:19 +0800

    7.16

---
+++ Project_Alibaba_workload/E50_Alibaba_cluster_predict_compare/Plot_MSE_via_excel/plot_mse.py
@@ -0,0 +1,19 @@
+#-*-coding:utf-8-*-
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from scipy import interpolate
+file = pd.read_csv(
+    '/Users/mac/Downloads/Plot_MSE_via_Matlab/mse_plot.csv',
+    header = None,
+    names = ['x', 'value'])
+data = pd.DataFrame(file)
+# 数组切片
+x = data['x']  # 取第一列数据
+y = data['value']  # 取第二列数据
+plt.xlim((0, 60))
+plt.plot(x,y)
+plt.savefig('out3.png',dpi=600)
+# 设置需要保存图片的分辨率
+plt.show()
+
