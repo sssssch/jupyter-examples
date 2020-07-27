ab4b8c9871102e18056328161f8897d10a29ec0e
Kminassch <s18807016698@outlook.com>
2020-07-27 19:43:22 +0800

    7.27

--- Project_google_task_usage/task_uasge_500_preprocess/data_inverse.py
+++ Project_google_task_usage/task_uasge_500_preprocess/data_inverse.py
@@ -1,7 +1,6 @@
 # -*-coding:utf-8-*-
 import pandas as pd
 from numpy import *
-import numpy as np
 
 dataset = pd.read_csv(
     'test_data.csv', header=None)
--- Project_google_task_usage/task_usage_total_preprocess/data_total_preprocessing.py
+++ Project_google_task_usage/task_usage_total_preprocess/data_total_preprocessing.py
@@ -1,7 +1,7 @@
 # -*-coding:utf-8-*-
 
 import pandas as pd
-import numpy as np
+from numpy import *
 
 name = [
     'start_time',
@@ -25,12 +25,18 @@ name = [
     'aggregation_type',
     'sampled_cpu_usage']
 
-dataset = pd.read_csv('task_usage_headed.csv', header=None, names=name)
+dataset = pd.read_csv(
+    'task_usage.csv',
+    header=None,
+    names=name,
+    nrows=10000000)
 
 dataset.drop('maximum_disk_io_time', axis=1, inplace=True)
 dataset.drop('cpi', axis=1, inplace=True)
 dataset.drop('aggregation_type', axis=1, inplace=True)
 dataset.drop('sample_portion', axis=1, inplace=True)
+dataset.drop('mai', axis=1, inplace=True)
+dataset.drop('unmapped_page_cache_memory_usage', axis=1, inplace=True)
 
 dataset.columns = [
     'start_time',
@@ -41,13 +47,11 @@ dataset.columns = [
     'mean_cpu_usage_rate',
     'canonical_memory_usage',
     'assigned_memory_usage',
-    'unmapped_page_cache_memory_usage',
     'total_page_cache_memory_usage',
     'maximum_memory_usage',
     'mean_disk_io_time',
     'mean_local_disk_space_used',
     'maximum_cpu_usage',
-    'mai',
     'sampled_cpu_usage']
 print(dataset.head())
 
@@ -66,13 +70,11 @@ et = et / 1000000
 mcur = dataset['mean_cpu_usage_rate']
 cmu = dataset['canonical_memory_usage']
 amu = dataset['assigned_memory_usage']
-upcmu = dataset['unmapped_page_cache_memory_usage']
 tpcmu = dataset['total_page_cache_memory_usage']
 mmu = dataset['maximum_memory_usage']
 mdit = dataset['mean_disk_io_time']
 mldsu = dataset['mean_local_disk_space_used']
 mcu = dataset['maximum_cpu_usage']
-mai = dataset['mai']
 scu = dataset['sampled_cpu_usage']
 
 col_len = len(st)
@@ -81,33 +83,32 @@ col_len = len(st)
 end_num = int(et[col_len - 1])
 
 # 创建一个长度与数据长度相同的数组，作为cpu的储存器
-cpu = [0] * (end_num - 600)
-cmui = [0] * (end_num - 600)
-amui = [0] * (end_num - 600)
-upcmui = [0] * (end_num - 600)
-tpcmui = [0] * (end_num - 600)
-mmui = [0] * (end_num - 600)
-mditi = [0] * (end_num - 600)
-mldsui = [0] * (end_num - 600)
-mcui = [0] * (end_num - 600)
-maii = [0] * (end_num - 600)
-scui = [0] * (end_num - 600)
+en_mat_lenth = end_num - 600
+cpu = [0] * en_mat_lenth
+cmui = [0] * en_mat_lenth
+amui = [0] * en_mat_lenth
+tpcmui = [0] * en_mat_lenth
+mmui = [0] * en_mat_lenth
+mditi = [0] * en_mat_lenth
+mldsui = [0] * en_mat_lenth
+mcui = [0] * en_mat_lenth
+scui = [0] * en_mat_lenth
 # 将cpu循环赋值，需要以行为单位，将每一行的cpu信息添加至cpu数组中
 # 这里的i代表着index
 # 意味着我需要读取对应的第i行数据，再将其赋值到cpu对应的，从st到et的数组中
 for i in range(0, col_len - 1):
     # 在这里，还需要将cpu的数组位置进行调整
-    cpu[int(st[i]) - 600:int(2 * et[i] - st[i] - 900)] += mcur[i]
-    cmui[int(st[i]) - 600:int(2 * et[i] - st[i] - 900)] += cmu[i]
-    amui[int(st[i]) - 600:int(2 * et[i] - st[i] - 900)] += amu[i]
-    upcmui[int(st[i]) - 600:int(2 * et[i] - st[i] - 900)] += upcmu[i]
-    tpcmui[int(st[i]) - 600:int(2 * et[i] - st[i] - 900)] += tpcmu[i]
-    mmui[int(st[i]) - 600:int(2 * et[i] - st[i] - 900)] += mmu[i]
-    mditi[int(st[i]) - 600:int(2 * et[i] - st[i] - 900)] += mdit[i]
-    mldsui[int(st[i]) - 600:int(2 * et[i] - st[i] - 900)] += mldsu[i]
-    mcui[int(st[i]) - 600:int(2 * et[i] - st[i] - 900)] += mcu[i]
-    maii[int(st[i]) - 600:int(2 * et[i] - st[i] - 900)] += mai[i]
-    scui[int(st[i]) - 600:int(2 * et[i] - st[i] - 900)] += scu[i]
+    isti_600 = int(st[i]) - 600
+    ieti_900 = int(2 * et[i] - st[i] - 900)
+    cpu[isti_600:ieti_900] += mcur[i]
+    cmui[isti_600:ieti_900] += cmu[i]
+    amui[isti_600:ieti_900] += amu[i]
+    tpcmui[isti_600:ieti_900] += tpcmu[i]
+    mmui[isti_600:ieti_900] += mmu[i]
+    mditi[isti_600:ieti_900] += mdit[i]
+    mldsui[isti_600:ieti_900] += mldsu[i]
+    mcui[isti_600:ieti_900] += mcu[i]
+    scui[isti_600:ieti_900] += scu[i]
 
 # 在这里，我将对生成的cpu数据进行划分，使得cpu = cpui / 当前时刻工作机器数量
 # for i in range(0, col_len - 1):
@@ -121,27 +122,43 @@ for i in range(0, col_len - 1):
 cpu = list(map(lambda x: x * (end_num / 300) / col_len, cpu))
 cmui = list(map(lambda x: x * (end_num / 300) / col_len, cmui))
 amui = list(map(lambda x: x * (end_num / 300) / col_len, amui))
-upcmui = list(map(lambda x: x * (end_num / 300) / col_len, upcmui))
 tpcmui = list(map(lambda x: x * (end_num / 300) / col_len, tpcmui))
 mmui = list(map(lambda x: x * (end_num / 300) / col_len, mmui))
 mditi = list(map(lambda x: x * (end_num / 300) / col_len, mditi))
 mldsui = list(map(lambda x: x * (end_num / 300) / col_len, mldsui))
 mcui = list(map(lambda x: x * (end_num / 300) / col_len, mcui))
-maii = list(map(lambda x: x * (end_num / 300) / col_len, maii))
 scui = list(map(lambda x: x * (end_num / 300) / col_len, scui))
-print(cpu)
 
-lissi = np.zeros((11, (end_num - 600)))
+lissi = np.zeros((9, (end_num - 600)))
 lissi[0] = cpu
 lissi[1] = cmui
 lissi[2] = amui
-lissi[3] = upcmui
-lissi[4] = tpcmui
-lissi[5] = mmui
-lissi[6] = mditi
-lissi[7] = mldsui
-lissi[8] = mcui
-lissi[9] = maii
-lissi[10] = scui
+lissi[3] = tpcmui
+lissi[4] = mmui
+lissi[5] = mditi
+lissi[6] = mldsui
+lissi[7] = mcui
+lissi[8] = scui
 
-np.savetxt("test.csv", lissi, delimiter=',')
+# np.savetxt("test.csv", lissi, delimiter=',')
+
+dataset = round(lissi, 8)
+List_data = mat(dataset)
+Inverse = List_data.T
+name = [
+    'cpu',
+    'cmui',
+    'amui',
+    'tpcmui',
+    'mmui',
+    'mditi',
+    'mldsui',
+    'mcui',
+    'scui'
+]
+
+test = pd.DataFrame(columns=name, data=Inverse)
+test.to_csv(
+    'task_usage_total_inversed_bycode.csv',
+    encoding='gbk',
+    header=None)
